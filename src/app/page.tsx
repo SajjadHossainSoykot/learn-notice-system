62f0871b644f1003b61f3a49ad75b8ca8ff1a704
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-05-23 03:35:30 +0600

    update Home page.tsx with notice link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,6 @@ export default function Home() {
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="/notices"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Go to Notices
           </a>
