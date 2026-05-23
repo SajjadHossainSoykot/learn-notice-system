106631c8d01ef4a0a36541b28c10b7bc016f9b87
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-05-24 00:17:41 +0600

    update layout.tsx with AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "./components/providers/AuthProvider";
 import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
@@ -29,8 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <AuthProvider>
         <Navbar/>
         {children}
+        </AuthProvider>
         </body>
     </html>
   );
