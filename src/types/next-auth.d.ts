89f09a73eda71aee45078bcf193aded7b166340e
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-05-24 04:04:16 +0600

    update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,16 +3,25 @@ import "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id?: string;
       name?: string | null;
       email?: string | null;
       image?: string | null;
       role?: string;
+      status?: string;
     };
+  }
+
+  interface User {
+    role?: string;
+    status?: string;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
+    id?: string;
     role?: string;
+    status?: string;
   }
 }
